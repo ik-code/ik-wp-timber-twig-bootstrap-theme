9c5d894be35535ebb593064aaa15797ab256ecb0
ik-code <igo.metacon@gmail.com>
2021-05-16 20:27:14 +0300

    footer

--- functions.php
+++ functions.php
@@ -84,9 +84,10 @@ class StarterSite extends Timber\Site {
 		$context['foo']   = 'bar';
 		$context['stuff'] = 'I am a value set in your functions.php file';
 		$context['notes'] = 'These values are available everytime you call Timber::context();';
-		$context['menu']  = new Timber\Menu();
-		$context['site']  = $this;
+		$context['menu']  = new Timber\Menu(16);
+		$context['secondary_menu'] = new Timber\Menu(19);
 		$context['art']  = 200;
+		$context['site'] = $this;
 		return $context;
 	}
 
